548a5d653d7b8ba72292be76ffac465a27820284
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-07-11 13:33:44 +0100

    Use json instead of text file

--- generate_input_file.py
+++ generate_input_file.py
@@ -1,4 +1,5 @@
+import json
 def generate_input_file(input_data: list):
     """Generate an input file, without input_file_generator"""
-    with open("input_file.txt", "w") as f:
-        f.write(" ".join(input_data))
+    with open("input_file.json", "w") as f:
+        json.dump(input_data, f, indent=2)
