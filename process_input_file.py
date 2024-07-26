079361c3fab68991c7a1ccc9746f40f4e7670aa1
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-07-26 14:14:39 +0100

    Open input file for reading instead of writing

--- process_input_file.py
+++ process_input_file.py
@@ -1,7 +1,7 @@
 import json
 def process_input_file():
     """Process an input file"""
-    with open("input_file.json", "w") as f:
+    with open("input_file.json", "r") as f:
         data = json.load(f)
     data = data * 2
     with open("processed_file.json", "w") as f:
